3be13c729eea79901489a8221d4cdc2e014ae225
adisupiansah <adisupiansyah15@gmail.com>
2025-01-24 15:06:19 +0700

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import NavigasiBar from '@/components/comp-mainpage/Navbar/NavigasiBar'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <NavigasiBar/>
+    </div>
+  )
+}
+
+export default page
